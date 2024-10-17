20ae9da7116c21c646e63cdc94fe11d71dd438c5
Mansour Fall <mansour.fall@gebeya.com>
2024-10-17 23:01:56 +0000

    feat: contact us body ok

--- projects/demo/src/app/config.ts
+++ projects/demo/src/app/config.ts
@@ -70,6 +70,50 @@ export const defaultConfig:ComponentConfig[] = [
     color: '#ffffff',
   },
   {
+    id: 'about-us-primary',
+    type: '_container',
+    items: ['about-us-primary-heading'],
+  },
+  {
+    id: 'about-us-primary-heading',
+    type: 'primary-heading',
+    text: 'About Us',
+    color: '#000000',
+  },
+  {
+    id: 'about-us-secondary',
+    type: '_container',
+    items: ['about-us-secondary-heading'],
+  },
+  {
+    id: 'about-us-secondary-heading',
+    type: 'secondary-heading',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    color: '#000000',
+  },
+  {
+    id: 'about-us-content',
+    type: '_container',
+    items: ['about-us-content-paragraph'],
+    classes: 'flex-column',
+  },
+  {
+    id: 'about-us-content-paragraph',
+    type: 'paragraph',
+    text: 'Sed do eiusmod tempor darko vador incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud. Lumen do eiusmod ator incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud. Lorem ipsum etc do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud',
+    color: '#000000',
+  },
+  {
+   id: 'featured-image',
+    type: '_container',
+    items: ['about-us-featured-image'],
+  },
+  {
+    id: 'about-us-featured-image',
+    type: 'image',
+    src: 'https://s3-alpha-sig.figma.com/img/fe3e/13a7/6ce3d2e6eda966b37b643d639baa140f?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jJYYWYsKSOvorPMIm~dAgFKLZibMOlr5aQwy5hxn9HwnzQk6ZUB8j25YViv5aCYSEScR~5PnZ1aBGo3K9OQWUiEqqt-vx-~ZRZxV1rI-4SZD6jeyASXb75YH5WqeiBrNGWCcSA1R5U-zqWesp5rHg-jbxHghEXiKP32m4hoSlPX4bLajf4BeNVkzoEYtRASmP-9tCF1riIEiJtv7eIk0jPfTdZiP-872nRICS3pXuHlQYZ4jGM1zWo4GUSSj2ZpDDrdJp8YFJwuLIxETzO--hg3OopVVKzw1A5ptDPyUzSn2TZb-lhUb2uW~r6RuoHnlOHmpFQzc4SFdM5Robn0vkw__',
+  },
+  {
     id: 'footer',
     type: '_container',
     items: ['title'],
